8659ed9a86df615efb05d87151f79833e3cfb46e
alvics <alvics71@hotmail.com>
2019-07-18 10:47:58 +1000

    add summernote wysiwyg/remove username from create new post

--- middleware/storePost.js
+++ middleware/storePost.js
@@ -1,11 +1,6 @@
 // Required fields for Post form
 module.exports = (req, res, next) => {
-  if (
-    !req.files.image ||
-    !req.body.username ||
-    !req.body.subtitle ||
-    !req.body.content
-  ) {
+  if (!req.files.image || !req.body.subtitle || !req.body.content) {
     return res.redirect('/posts/new');
   }
 
