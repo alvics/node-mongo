43f677e5ec270c77b4d8e563ff7fc968e23a13ec
alvics <alvics71@hotmail.com>
2019-07-19 10:29:10 +1000

    add catch() for unhandled promise

--- controllers/getPost.js
+++ controllers/getPost.js
@@ -2,7 +2,13 @@ const Post = require('../database/models/Post');
 
 module.exports = async (req, res) => {
   const post = await Post.findById(req.params.id).populate('author');
-  res.render('post', {
-    post
-  });
+  res
+    .render('post', {
+      post
+    })
+    .catch(error =>
+      process.on('unhandledRejection', (reason, promise) => {
+        promise.reject(typeof error === 'string' ? error : error.message);
+      })
+    );
 };
--- controllers/homePage.js
+++ controllers/homePage.js
@@ -4,7 +4,13 @@ module.exports = async (req, res) => {
   const posts = await Post.find({}).populate('author');
   console.log(posts);
 
-  res.render('index', {
-    posts
-  });
+  res
+    .render('index', {
+      posts
+    })
+    .catch(error =>
+      process.on('unhandledRejection', (reason, promise) => {
+        promise.reject(typeof error === 'string' ? error : error.message);
+      })
+    );
 };
